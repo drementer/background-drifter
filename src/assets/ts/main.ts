2fba15efaea29f77084066eaa4dfa93bd8174ad7
Burak <48869708+drementer@users.noreply.github.com>
2025-08-23 14:22:40 +0300

    Enhance overlay functionality by adding 'open-overlay' attribute and implementing hideOverlay function for improved user experience

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,7 +1,9 @@
 import { createTextAnimation } from './features/textAnimate';
 import { createParallaxBackground } from './features/parallaxBackground';
 import { createMouseParallax } from './features/parallaxMouse';
+import { hideOverlay } from './features/overlayAnimation';
 
 createTextAnimation();
 createParallaxBackground();
 createMouseParallax();
+hideOverlay();
